dbc29926d8651c59231dcf41a69cba382e02b396
GoldenFox013 <borok.maja@gmail.com>
2025-01-23 23:35:42 +0100

    fájlbeolvasás classokkal és függvényekkel
    spicy content

--- muveszbolt.py
+++ muveszbolt.py
@@ -1,34 +1,48 @@
-lista = []
+class Termekek:
+    def __init__(self, nev, ar, db, ev):
+        self.nev = nev
+        self.ar = int(ar)
+        self.db = int(db)
+        self.ev = int(ev)
 
-with open("termekek.txt", "r", encoding="UTF-8") as file:
-    elsosor = file.readline()       # ha van a forrásfájlban első fölösleges sor
-    for sor in file:
-        adat = sor.strip().split(";")
-        lista.append([adat[0], int(adat[1]),int(adat[2]), int(adat[3])])
+    def __str__(self):
+        return f"{self.nev};{self.ar};{self.db};{self.ev}"
+    
+def beolvasas(txt):
+    tomb = []
+    with open(txt, "r", encoding="UTF-8") as file:
+        elsosor = file.readline()       # ha van első felesleges sor
+        for sor in file:
+            adat = sor.strip().split(";")
+            tomb.append(Termekek(adat[0], adat[1], adat[2], adat[3]))
+    return tomb
+
+lista = beolvasas("termekek.txt")
+# ha kérne valami ilyesmit:
+# print("A fájl tartalma " + "\n".join(map(str,lista)))
 
 print("A fájl tartalma:")
 for i in lista:
-    print(f"{i[0]};{i[1]};{i[2]};{i[3]}")
+    print(i)
 
+# az összes feladat egy ciklusban:
 draga = lista[0]
-for i in lista:
-    if draga[1] < i[1]:
-        draga = i
-print(f"\nA legdrágább termék: {draga[0]} ({draga[1]} Ft)")
-
 ossz = 0
-for i in lista:
-    ossz += i[2]
-print(f"Az eladott termékek darabszámának átlaga: {round(ossz / len(lista), 1)} Ft")
-
 C_sek = []
 for i in lista:
-    if i[0][0] == "C":      # VAGY if i[0].startswith("C"):
+    ossz += i.db
+
+    if draga.ar < i.ar:
+        draga = i
+    
+    if i.nev[0] == "C":
         C_sek.append(i)
 
-print(f"A C-vel kezdődő termékek száma: {len(C_sek)}")
+print(f"\nA legdrágább termék: {draga.nev} ({draga.ar} Ft)")
+print(f"Az eladott termékek darabszámának átlaga: {round(ossz / len(lista), 1)}")
+print(f'"C"-vel kezdődő termékek száma: {len(C_sek)}')
 
+print('"C"-vel kezdődő termékek:')
 with open("C_betus_termekek.txt", "w", encoding="UTF-8") as fajl:
-    for i in C_sek:
-        print(f"{i[0]};{i[1]};{i[2]};{i[3]}")
-        fajl.write(f"{i[0]};{i[1]};{i[2]};{i[3]}\n")
+    fajl.write("\n".join(map(str,C_sek)))
+    print("\n".join(map(str,C_sek)))
